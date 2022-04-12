07c9a2af966b4092d695f11fe5d169e7ee6c3022
Sarah Younis <SarahYO@exalt.ps>
2022-04-12 15:14:11 +0300

    test

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -2,12 +2,20 @@ package com.example.taskmanager.repository;
 
 import com.example.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
     void deleteAllByUser_Id(Long id);
     List<Task> findAllByUser_Id(Long id);
-    //No code is needed CRUD functions already exist within JPARepository
+    //function to find tasks for specific user but not the completed ones
+    @Query("select a from Task a where a. <= :creationDateTime")
+    List<Task> findAllWithCreationDateTimeBefore(@Param("creationDateTime") Date creationDateTime);
+
 }
+    //No code is needed CRUD functions already exist within JPARepository
+
